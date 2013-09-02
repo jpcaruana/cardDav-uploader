38e45d23f4993eaa10990e079ba69b4ff1deb35b
Jean-Philippe CARUANA <jp@barreverte.fr>
2013-09-02 20:24:18 +0200

    1er pas : poster une vcard

---
+++ uploader.py
@@ -0,0 +1,86 @@
+import requests
+import getpass
+import shutil
+from numbers import Number
+from httplib import responses as HTTP_CODES
+from urlparse import urlparse
+import xml.etree.cElementTree as xml
+from collections import namedtuple
+from cStringIO import StringIO
+
+class OperationFailed(Exception):
+    _OPERATIONS = dict(
+        GET = "download",
+        PUT = "upload",
+        DELETE = "delete",
+        MKCOL = "create directory",
+        PROPFIND = "list directory",
+        )
+    def __init__(self, method, path, expected_code, actual_code):
+        self.method = method
+        self.path = path
+        self.expected_code = expected_code
+        self.actual_code = actual_code
+        operation_name = self._OPERATIONS[method]
+        self.reason = 'Failed to {operation_name} "{path}"'.format(**locals())
+        expected_codes = (expected_code,) if isinstance(expected_code, Number) else expected_code
+        expected_codes_str = ", ".join('{0} {1}'.format(code, str(code)) for code in expected_codes)
+        actual_code_str = str(actual_code)
+        msg = '''\
+{self.reason}.
+  Operation     :  {method} {path}
+  Expected code :  {expected_codes_str}
+  Actual code   :  {actual_code} {actual_code_str}'''.format(**locals())
+        super(OperationFailed, self).__init__(msg)
+
+
+class Client(object):
+
+    def __init__(self, host, port=0, username=None, password=None, protocol='http'):
+        if not port:
+            port = 443 if protocol == 'https' else 80
+        self.baseurl = '{0}://{1}:{2}'.format(protocol, host ,port)
+        self.session = requests.session()
+        if username and password:
+            self.session.auth = (username, password)
+        print "Connecting to %s with user %s" % (self.baseurl, username)
+
+    def _send(self, method, path, expected_code, **kwargs):
+        url = self._get_url(path)
+        print url
+        response = self.session.request(method, url, allow_redirects=False, **kwargs)
+        print response.text
+        if isinstance(expected_code, Number) and response.status_code != expected_code \
+            or not isinstance(expected_code, Number) and response.status_code not in expected_code:
+            raise OperationFailed(method, path, expected_code, response.status_code)
+        return response
+
+    def _get_url(self, path):
+        return self.baseurl + str(path).strip()
+
+    def add_card(self, card):
+        expected_codes = [201]
+        path = '/card.php/addressbooks/jp/default/example.vcf'
+        self._send('PUT', path, expected_codes, data=card, headers={'Content-Type': 'text/vcard', 'If-None-Match': '*'})
+
+if __name__ == '__main__':
+    server   = raw_input('enter server url: ')
+    user     = raw_input('enter username  : ')
+    password = getpass.getpass()
+
+    client = Client(server, username=user, password=password)
+    card = """BEGIN:VCARD
+VERSION:3.0
+FN:Toto tutu
+N:tutu;toto
+ADR;TYPE=POSTAL:;2822 Email HQ;Suite 2821;RFCVille;PA;15213;USA
+EMAIL;TYPE=INTERNET,PREF:toto@example.com
+NICKNAME:the other one
+NOTE:Example VCard 2.
+ORG:Inc Copr
+TEL;TYPE=WORK,VOICE:412 605 0499
+TEL;TYPE=FAX:412 605 0705
+URL:http://www.example.co.uk
+UID:1234-5678-9000-2
+END:VCARD"""
+    client.add_card(card)    
