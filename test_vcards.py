ec17f42d1ce8def3299d83459f01ede822c9ec1b
Jean-Philippe Caruana <jp@barreverte.fr>
2013-09-05 12:57:50 +0200

    parse several vcards

--- test_vcards.py
+++ test_vcards.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from nose.tools import assert_equal
-from vcards import Card
+from nose.tools import assert_equal, assert_in
+from vcards import Card, VcardParser
 
-CARD = """BEGIN:VCARD
+TOTO_CARD = """BEGIN:VCARD
 VERSION:3.0
 FN:Toto tutu
 N:tutu;toto
 ADR;TYPE=POSTAL:;2822 Email HQ;Suite 2821;RFCVille;PA;15213;USA
 EMAIL;TYPE=INTERNET,PREF:toto@example.com
 NICKNAME:the other one
-NOTE:Example VCard 2.
+NOTE:Example VCard.
 ORG:Inc Copr
 TEL;TYPE=WORK,VOICE:412 605 0499
 TEL;TYPE=FAX:412 605 0705
@@ -17,8 +17,22 @@ URL:http://www.example.co.uk
 UID:1234-5678-9000-2
 END:VCARD"""
 
+JOHN_CARD = """BEGIN:VCARD
+VERSION:3.0
+FN:John Doe
+N:Doe;John
+NICKNAME:john
+NOTE:Example VCard 2.
+URL:http://www.example.fr
+END:VCARD"""
+
 class TestVcard(object):
-    def test_card(self):
-        card = Card(CARD)
-        assert_equal(CARD, card.content)
+    def test_create_a_vcard(self):
+        card = Card(TOTO_CARD)
+        assert_equal(TOTO_CARD, card.content)
         assert_equal('tutu.toto.vcf', card.title)
+
+    def test_parse_2_vcards_in_one_file(self):
+        cards = VcardParser().parse(TOTO_CARD + '\n' + JOHN_CARD)
+        assert_in(Card(TOTO_CARD), cards)
+        assert_in(Card(JOHN_CARD), cards)
--- vcards.py
+++ vcards.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+class VcardParser(object):
+    def parse(self, vcards):
+        cards = []
+        lines = vcards.split('\n')
+        one_card = []
+        for line in lines:
+            one_card.append(line)
+            if line == 'END:VCARD':
+                cards.append(Card('\n'.join(one_card)))
+                one_card = []
+        return cards
+
+
 class Card(object):
     def __init__(self, content):
         self.content = content
@@ -9,3 +22,10 @@ class Card(object):
         for line in lines:
             if line.startswith("N:"):
                 return line.replace('N:', '').replace(';', '.') + ".vcf"
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) \
+                   and self.content == other.content and self.title == other.title
+
+    def __repr__(self):
+        return self.title + ":\n" + self.content
